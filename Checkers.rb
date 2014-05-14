52bf04b0ccd193f5cc776638f996ee250127808c
App Academy Student <student@appacademy.io>
2014-05-14 14:08:57 -0400

    Add HumanPlayer Class and 'initialize method' to Chess

--- Checkers.rb
+++ Checkers.rb
@@ -2,10 +2,15 @@
 
 class Checkers
   
-  attr_reader
+  attr_reader :board, :current_player, :players
   
   def intialize
-    
+    @board = Board.new
+    @players = {
+      :white = HumanPlayer.new(:white)
+      :black = HumanPlayer.new(:black)
+    }
+    @current_player = :white
   end
   
   def play_game
@@ -15,6 +20,19 @@ class Checkers
   private
   
   def helper_method
+    
+  end
+end
+
+class HumanPlayer
+  
+  attr_reader :color
+  
+  def initialize(color)
+    @color = color
+  end
+  
+  def get_move
     
   end
 end
